a42cc88cf9399e0707cc8e11329e4afa824272c0
Nathan Bell <nathanbell09@gmail.com>
2018-11-10 18:08:16 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -93,7 +93,7 @@ def big_shoe_rebounds
       end
     end
   end
-
+  num_rebounds
   # shoe_sizes = []
   # game_hash.each do |location, team_info|
   #   team_info[:players].each do |name, statistics|
